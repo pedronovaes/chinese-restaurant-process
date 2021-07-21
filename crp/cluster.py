f6b7add85951cb79e76f8e01f6920ceed440ded8
pedronovaes <pedronovaesmelo@gmail.com>
2021-07-21 14:24:33 -0300

    Initial CRP structure

---
+++ crp/cluster.py
@@ -0,0 +1,12 @@
+class ChineseRestaurantProcess:
+    def __init__(self, distance_threshold=0.8):
+        pass
+
+    def fit(self, X):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def fit_predict(self, X):
+        pass
